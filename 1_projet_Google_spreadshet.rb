810daa78ba94fa1aed4c3c552df862f94273ecaf
isarceaux <isarceaux>
2017-07-17 16:19:25 +0200

    tentative de suppression du fichier json

--- 1_projet_Google_spreadshet.rb
+++ 1_projet_Google_spreadshet.rb
@@ -1,27 +1,20 @@
 require "rubygems"
 require "google_drive"
 require "json"
-
 require_relative "1_route_de_la_mairie"
 
-# Creates a session. This will prompt the credential via command line for the
-# first time and save it to config.json file for later usages.
-# See this document to learn how to create config.json:
-# https://github.com/gimite/google-drive-ruby/blob/master/doc/authorization.md
-#session = GoogleDrive::Session.from_config("config.json")
-
-# First worksheet of
-# https://docs.google.com/spreadsheet/ccc?key=pz7XtlQC-PYx-jrVMJErTcg
-# Or https://docs.google.com/a/someone.com/spreadsheets/d/pz7XtlQC-PYx-jrVMJErTcg/edit?usp=drive_web
-# https://docs.google.com/spreadsheets/d/1Cy87fQ3hsZAuDPvmzIHxi5UfFc1OmKGXiISlM-V8EGs/edit?usp=drive_web
-#ws = session.spreadsheet_by_key("1Cy87fQ3hsZAuDPvmzIHxi5UfFc1OmKGXiISlM-V8EGs").worksheets[0]
-
-
-def perform1
-	hash_towns = get_emails_from_val_doise
-	towns = hash_towns.keys
+def setup_spreadsheet
 	session = GoogleDrive::Session.from_config("config.json")
 	ws = session.spreadsheet_by_key("1Cy87fQ3hsZAuDPvmzIHxi5UfFc1OmKGXiISlM-V8EGs").worksheets[0]
+end
+
+def get_the_hash_that_I_scrapped_few_days_ago
+	return get_emails_from_val_doise
+end
+
+def put_data_in_spreadsheet(hash_towns)
+	setup_spreadsheet
+	towns = hash_towns.keys
 	i=1
 	towns.each do |town|
 		ws[i, 1] = town
@@ -31,4 +24,14 @@ def perform1
 	end
 end
 
-perform2()
+def put_data_in_json(hash_towns)
+	File.open("results/town_scrapping.json","w") do |f|
+		f.write(hash_towns.to_json)
+	end
+end
+
+def perform
+	hash_towns = get_the_hash_that_I_scrapped_few_days_ago
+	#put_data_in_spreadsheet(hash_towns)
+	put_data_in_json(hash_towns)
+end
