1825fb958143cf427b3a95e996dacbbd7571bd6d
isarceaux <isarceaux>
2017-07-18 12:42:03 +0200

    ajout de la fonction pour envoyer un mail à etienne! mail pas encore reçu...

--- 2_gmail_API.rb
+++ 2_gmail_API.rb
@@ -1,10 +1,28 @@
 require 'gmail'
+#require 'gmail_xoauth'
 require_relative 'config_gmail'
+require "json"
+#require 'client_id.json'
+require 'omniauth-google-oauth2'
 
-gmail = Gmail.connect(username, password)
+variables = get_variables
+
+gmail = Gmail.connect(:xoauth2,variables[0], variables[1])
 # play with your gmail...
 
-puts .inbox.count
+gmail.deliver do
+  to "etienne@gmx.fr"
+  subject "Mon premier test d'envoi de mail avec l'API!"
+  text_part do
+    body "Bon je ne sais pas trop quoi raconter, lol."
+  end
+  html_part do
+    content_type 'text/html; charset=UTF-8'
+    body "<p>Text of <em>html</em> message.</p>"
+  end
+  add_file "/path/to/some_image.jpg"
+end
+
 
 gmail.logout
 
--- titelize.rb
+++ titelize.rb
@@ -4,7 +4,7 @@ def titleize(title) #WIP
 	title.split.each do |word|
 		if new_title == ""
 			new_title << word.capitalize
-		elsif ["et", "sur", "le", "en","les","la","sous","dit","l"].include? word.downcase
+		elsif ["et", "sur", "le", "en","les","la","sous","dit","l","aux","1","2","3","4","5","6","7","8","9"].include? word.downcase
 			new_title << " "
 			new_title << word.downcase
 		else	
