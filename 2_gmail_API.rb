48a0b8f33ffb515f990244939672c8083750e40a
isarceaux <isarceaux>
2017-07-18 16:51:45 +0200

    Mail rédigé et testé sur les adresses mails bidons

--- 2_gmail_API.rb
+++ 2_gmail_API.rb
@@ -9,8 +9,6 @@ require 'omniauth-google-oauth2'
 def send_email(email,mail_content,mail_subject)
 	variables = get_variables
 	gmail = Gmail.connect(variables[0], variables[1])
-	puts gmail.logged_in?
-
 	gmail.deliver do
 	  to email
 	  subject mail_subject
@@ -19,40 +17,49 @@ def send_email(email,mail_content,mail_subject)
 	  #end
 	  html_part do
 	    content_type 'text/html; charset=UTF-8'
-	    body "<p>Text of <em>html</em> message.</p>"
+	    body mail_content
+	    puts "Mail sent to #{email}"
 	  end
 	end
-
 	gmail.logout
 end
 
 def get_email_from_spreadsheet(key)
-	email_list=[]
+	email_hash={}
 	i=2
 	session = GoogleDrive::Session.from_config("config.json") #ligne pour se connecter à l'API google drive
 	ws = session.spreadsheet_by_key(key).worksheets[0] #ligne pour ouvrir la spreadsheet
 	ws.num_rows.times do 
-		email_list << ws[i,2]
+		email_hash[ws[i,1]] = ws[i,2]
 		i+=1
 	end
-	return email_list
+	return email_hash
+end
+
+def mail_writing(town)
+
+	content = "
+	<p>Cher(e) Maire,</p>
+	<p>Nous nous permettons de vous contacter dans le cadre de la formation que nous suivons : <a href='http://thehackingproject.org/'>The Hacking Project</a>.</p>
+	<p>Il s'agit d'une formation <em>gratuite et ouverte à tous au développement web</em> dont l'objectif est de nous permettre de <em>décrocher un emploi ou de monter notre propre entreprise </em>!
+	J'ai personnellement déjà beaucoup appris, car je vous envoie ce mail grâce à un robot que j'ai écrit moi-même !</p>
+	<p>Les organisateurs de The Hacking Project sont à la recherche de nouvelles recrues motivées pour apprendre à coder, ainsi que de locaux pour travailler et de contacts bienveillants auprès d'associations et de mairies.</p>
+	<p>Si vous souhaitez les aider, afin de faire profiter à toujours plus d'administrés de cette belle oppportunité de développer de nouvelles compétences clés pour l'avenir, n'hésitez pas à contacter Charles, à l'origine de cet audacieux projet : 06 95 46 60 80.</p>
+	<p>Au plaisir d'échanger avec vous si vous souhaitez avoir des retours d'expérience, d'ici le 28 juillet.</p>
+	<p>Cordialement,</p>
+	<p>Marie Lucette</p>
+	"
+	return content
 end
 
 def perform
 	spreadsheet_key = "1WNoSQVMPjuOhqOC5cinuhctiR1p5yk4JLq6uyePDFN8"
 	#spreadsheet_key = "1PsqSfePTkHpeEKY9DyUKxe9bi_0xcEnaOfMyQd2_bjc"
-	email_list = get_email_from_spreadsheet(spreadsheet_key)
-
-	email_list.length.times do |mail_number|
-		puts email_list[mail_number]
-		content = "Kikou lol la mairie"
-		subject = "Yo test #2"
-		send_email(email_list[mail_number],content,subject)
+	email_hash = get_email_from_spreadsheet(spreadsheet_key)
+	email_hash.keys.each do |town|
+		mail_content = mail_writing(town)
+		send_email(email_hash[town],mail_writing(town),"A destination de #{town} - THP - RSVP avant le 28 juillet")
 	end
-	#email = "isabellecorp@gmail.com"
-	#content = "Bon je ne sais pas trop quoi raconter, lol."
-	#subject = "Mon 6ème test de mail"
-	#send_email(email,content,subject)
 	
 end
 
