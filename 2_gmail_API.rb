87c406fd0246ac445e1561fb5cfc32b685bc4697
isarceaux <isarceaux>
2017-07-18 15:16:51 +0200

    Envoi de mail via une fonction et passage en paramètre du mail, contenu et titre

--- 2_gmail_API.rb
+++ 2_gmail_API.rb
@@ -6,23 +6,34 @@ require "json"
 require 'omniauth-google-oauth2'
 
 
-variables = get_variables
-gmail = Gmail.connect(variables[0], variables[1])
-puts gmail.logged_in?
+def send_email(email,mail_content,mail_subject)
+	variables = get_variables
+	gmail = Gmail.connect(variables[0], variables[1])
+	puts gmail.logged_in?
 
-gmail.deliver do
-  to "isabellecorp@gmail.com"
-  subject "Mon deuxième test d'envoi de mail avec l'API depuis Marie Lucette!"
-  text_part do
-    body "Bon je ne sais pas trop quoi raconter, lol."
-  end
-  html_part do
-    content_type 'text/html; charset=UTF-8'
-    body "<p>Text of <em>html</em> message.</p>"
-  end
+	gmail.deliver do
+	  to email
+	  subject mail_subject
+	  text_part do
+	    body mail_content
+	  end
+	  #html_part do
+	  #  content_type 'text/html; charset=UTF-8'
+	  #  body "<p>Text of <em>html</em> message.</p>"
+	  #end
+	end
+
+	gmail.logout
 end
 
-gmail.logout
+def perform
+	email = "isabellecorp@gmail.com"
+	content = "Bon je ne sais pas trop quoi raconter, lol."
+	subject = "Mon 6ème test de mail"
+	send_email(email,content,subject)
+end
+
+perform
 
 
 
